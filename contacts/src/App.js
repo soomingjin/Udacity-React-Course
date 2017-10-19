8fb91ae6f0090c6c6cd9ea54ee797a561f14112f
soomingjin <mingjin_soo@mymail.sutd.edu.sg>
2017-10-19 19:09:11 +0800

    added router

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts';
+import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI';
 
 class App extends Component {
   state = {
+    screen : 'list',
     contacts: []
   };
   componentDidMount(){
@@ -19,8 +22,14 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
-        <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts}/>
+      <div className='app'>
+      <Route exact path="/" render={()=>(
+        <ListContacts
+          onDeleteContact={this.removeContact}
+          contacts={this.state.contacts}
+        />)}
+      />
+      <Route path='/create' component={CreateContact}/>
       </div>
     )
   }
