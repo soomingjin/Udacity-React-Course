d4ae5015adec67828f0480b16bd704fec6e38a40
soomingjin <mingjin_soo@mymail.sutd.edu.sg>
2017-12-01 19:31:57 +0800

    Type: Code Cleanup
    Converted Book component into a stateless functional component

--- Project1_MyReads/src/Book.js
+++ Project1_MyReads/src/Book.js
@@ -1,52 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-class Book extends React.Component {
-  static propTypes = {
-    data: PropTypes.object.isRequired,
-    key: PropTypes.string,
-    onBookChange: PropTypes.func.isRequired
-  }
-
-  handleChange = (e) => {
-    e.preventDefault();
-    this.props.onBookChange(this.props.data, e.target.value);
-  }
-
-  render() {
-    const data = this.props.data;
-    return (
-      <li>
-        <div className="book">
-          <div className="book-top">
-            <div className="book-cover" style={{
-                width: 128,
-                height: 193,
-                backgroundImage: `url(${data.imageLinks.thumbnail})`
-              }}></div>
-            <div className="book-shelf-changer">
-              {/**
-              Manipulate the value attribute on the root element to choose the selected element
-              These are Controlled Components.
-              In HTML, form elements such as <input>, <textarea>, and <select> typically maintain their own state and update it based on user input.
-              You can pass an array into the value attribute, allowing you to select multiple options in a select tag:
-              */
-              }
-              <select value={data.shelf} onChange={this.handleChange}>
-                <option value="none" disabled="disabled">Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
-          </div>
-          <div className="book-title">{data.title}</div>
-          <div className="book-authors">{data.author}</div>
+export default Book = ({data, onBookChange}) => (
+  <li>
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{
+            width: 128,
+            height: 193,
+            backgroundImage: `url(${data.imageLinks.thumbnail})`
+          }}></div>
+        <div className="book-shelf-changer">
+          {/**
+                  Manipulate the value attribute on the root element to choose the selected element
+                  These are Controlled Components.
+                  In HTML, form elements such as <input>, <textarea>, and <select> typically maintain their own state and update it based on user input.
+                  You can pass an array into the value attribute, allowing you to select multiple options in a select tag:
+                  */
+          }
+          <select value={data.shelf} onChange={(e) => onBookChange(data, e.target.value)}>
+            <option value="none" disabled="disabled">Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
-      </li>
-    )
-  }
-}
+      </div>
+      <div className="book-title">{data.title}</div>
+      <div className="book-authors">{data.author}</div>
+    </div>
+  </li>
+)
 
-export default Book
+Book.propTypes = {
+  data: PropTypes.object.isRequired,
+  key: PropTypes.string,
+  onBookChange: PropTypes.func.isRequired
+}
