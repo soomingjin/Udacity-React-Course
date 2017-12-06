945a43ab6b063ee946402a995be5c809feb53287
soomingjin <mingjin_soo@mymail.sutd.edu.sg>
2017-12-07 02:40:48 +0800

    mapDispatchToProps

--- udacimeals/src/components/App.js
+++ udacimeals/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { addRecipe, removeFromCalendar } from '../actions';
 
 class App extends Component {
   render() {
@@ -30,4 +31,12 @@ function mapStateToProps (calendar) {
   }
 }
 
-export default connect(mapStateToProps,)(App);
+// May not be that useful, only optimises code writing I guess..
+function mapDispatchToProps (dispatch){
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
